7b3eb28e6ab62c0a845c0cc32806e8cb6d952bcc
Anton <tohntobshi@Antons-MacBook-Pro.local>
2023-05-06 02:55:28 -0300

    created implementation of combo box

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 
@@ -16,6 +16,7 @@ const items: IItem[] = [
 ]
 
 interface IProps {
+    placeholder: string;
     items: IItem[];
     searchText: string;
     onSearchTextChange: (val: string) => void;
@@ -23,19 +24,70 @@ interface IProps {
     onItemChange: (val: IItem) => void;
 }
 
-function ComboBox(props: IProps) {
-    return <div>
-        todo
+function ComboBox({ placeholder, chosenItem, searchText, onSearchTextChange,
+    items, onItemChange }: IProps) {
+    const [isOpen, setIsOpen] = useState(false)
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    useEffect(() => {
+        const onDocumentClick = () => {
+            setIsOpen(false)
+        }
+        window.addEventListener('click', onDocumentClick)
+        return () => window.removeEventListener('click', onDocumentClick)
+    }, [])
+    const onOpenButtonClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation()
+        setIsOpen(true)
+        setTimeout(() => inputRef.current?.focus(), 100)
+    }
+    const onOpenContainerClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation()
+    }
+    const onItemClick = (item: IItem) => {
+        onItemChange(item)
+        setIsOpen(false)
+    }
+    return <div className='comboBoxContainer'>
+        <div className='openButton' onClick={onOpenButtonClick}>
+            {chosenItem ?
+                <span className='chosenText'>{chosenItem.text}</span> :
+                <span className='placeholder'>{placeholder}</span>
+            }
+            <img src='down-arrow.svg' className='arrowDown'/>
+        </div>
+        {isOpen && <div
+            className='openContainer'
+            onClick={onOpenContainerClick}>
+            <div className='inputContainer'>
+                <input
+                    placeholder={placeholder}
+                    value={searchText}
+                    onChange={e => onSearchTextChange(e.target.value)}
+                    className='input'
+                    ref={inputRef}
+                />
+                <img src='down-arrow.svg' className='arrowDown'/>
+            </div>
+            <div className='listContainer'>
+                {items.map(el => <div key={el.id} className='listElement'
+                    onClick={() => onItemClick(el)}>
+                    <span className='icon'>{el.icon}</span>
+                    <span>{el.text}</span>
+                </div>)}
+            </div>
+        </div>}
     </div>
 }
 
 function App() {
     const [searchText, setSearchText] = useState('')
     const [chosenItem, setChosenItem] = useState<IItem | undefined>()
-    const filteredItems = items.filter(el => el.text.includes(searchText))
+    const filteredItems = items.filter(el =>
+        el.text.toLowerCase().includes(searchText.toLowerCase()))
 
-    return <div className='abc'>
+    return <div className='rootContainer'>
         <ComboBox
+            placeholder='Choose a Fruit:'
             items={filteredItems}
             searchText={searchText}
             chosenItem={chosenItem}
